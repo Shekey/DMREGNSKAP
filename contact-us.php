8d08e858a42dbb572f10e90c8d9a094b216f5698
Sheky <ajdin_sah@hotmail.com>
2018-11-20 21:06:02 +0100

    php mailer

--- contact-us.php
+++ contact-us.php
@@ -1,4 +1,37 @@
-﻿<!DOCTYPE html>
+﻿<?php
+    // Import PHPMailer classes into the global namespace
+    // These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
+    $mail = new PHPMailer;
+    
+    $mail->isSMTP(); 
+    $mail->Host = 'mailcluster.loopia.se'; 
+    $mail->SMTPAuth = true; 
+    $mail->Username = 'dminfo@dmregnskap.no'; // SMTP username
+    $mail->Password = 'UjhacNuaj8'; // SMTP password
+    $mail->SMTPSecure = 'tls'; 
+    $mail->Port = 587;
+    
+    $mail->From = 'ajdinsheki@gmail.coim';
+    $mail->FromName = 'Ajdin';
+    $mail->addAddress('dminfo@dmregnskap.no', 'Info'); // Add a recipient
+    $mail->Subject = 'Here is the subject';
+    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+    if(!$mail->send()) {
+     echo 'Message could not be sent.';
+     echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+     echo 'Message has been sent';
+    }
+?>
+
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -19,7 +52,7 @@
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
 </head>
-
+<?php ?>
 <body>
 
     <!-- header -->
